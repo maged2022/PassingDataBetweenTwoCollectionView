d124b4df41695d3cf80192e860352a63b55d164b
maged mohamed <maged12345@Github.com>
2022-02-05 19:15:49 +0200

    adding collection view to second view controller

--- 3- passing using delegation/SecondViewController.swift
+++ 3- passing using delegation/SecondViewController.swift
@@ -11,6 +11,7 @@ protocol UserInfo {
 }
 
 class SecondViewController: UIViewController {
+    var myCollectionView:UICollectionView!
     var userInfoDelegate:UserInfo?
     var button :UIButton!
     var label:UILabel!
@@ -20,6 +21,7 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         setUpLabel()
         setUPButton()
+        setUpCollectionView ()
         label.text = name
        
         // Do any additional setup after loading the view.
@@ -49,5 +51,44 @@ class SecondViewController: UIViewController {
         userInfoDelegate?.userInfo(name: "maged mohammed", age: 30, color: .green)
         dismiss(animated: true, completion: nil)
     }
+    
+    func setUpCollectionView () {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: 60, height: 60)
+        layout.scrollDirection = .horizontal
+        //myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        let frame = CGRect(x: 0, y: 580, width: 400, height: 100)
+        myCollectionView =  UICollectionView(frame: frame, collectionViewLayout: layout)
+        
+        myCollectionView.dataSource = self
+        myCollectionView.delegate = self
+        
+        myCollectionView.register(MyCollectionViewCell.nib(), forCellWithReuseIdentifier: "MyCell")
+        myCollectionView.backgroundColor = UIColor.white
+        self.view.addSubview(myCollectionView)
+    }
 }
 
+
+extension SecondViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath) as! MyCollectionViewCell
+        myCell.myimage.image = UIImage(named: "maged")
+        return myCell
+    }
+    
+}
+
+extension SecondViewController : UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+    {
+        print("User tapped on item \(indexPath.row)")
+    }
+    
+    
+}
